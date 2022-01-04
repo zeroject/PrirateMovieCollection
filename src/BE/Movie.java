79cd30518bedb3266ce065fc3f63c9f4b18bf926
zeroject <47432671+zeroject@users.noreply.github.com>
2022-01-04 12:46:09 +0100

    Design
    fa

---
+++ src/BE/Movie.java
@@ -0,0 +1,7 @@
+package BE;
+
+public class Movie {
+    public Movie(){
+
+    }
+}
--- src/MVC/MainWindow.java
+++ src/MVC/MainWindow.java
@@ -1,5 +1,34 @@
 package MVC;
 
-public class MainWindow {
+import BE.Movie;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+public class MainWindow {
+    @FXML
+    private TableView<Movie> movieTableView;
+    @FXML
+    private TextField searchTextField;
+    @FXML
+    private ComboBox comboBox;
+
+    public MainWindow(){
+
+    }
+
+    public void debugConsole() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("View/Debug Console.fxml"));
+        Stage stage = new Stage();
+        stage.setResizable(false);
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
 }
