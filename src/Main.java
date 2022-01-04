2bed6503ba5e0fee4ff1029f6b3fb572c50f93cf
zeroject <47432671+zeroject@users.noreply.github.com>
2022-01-04 12:01:32 +0100

    Main Window Works

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MVC/View/MainWindow.fxml"));
+        Parent root = loader.load();
+        primaryStage.setTitle("Pirate Movies");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+        primaryStage.setResizable(false);
+    }
+
+    public static void main(String[] args) {
+        Application.launch();
+    }
+}
