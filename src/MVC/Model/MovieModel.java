1683c9c410acfb053883fa0068a84c5bf820c68a
kasp441 <91524039+kasp441@users.noreply.github.com>
2022-01-05 09:32:38 +0100

    crash update

--- src/MVC/Model/MovieModel.java
+++ src/MVC/Model/MovieModel.java
@@ -4,6 +4,9 @@ import BE.Movie;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MovieModel
 {
     private ObservableList<Movie> movieList;
@@ -11,4 +14,15 @@ public class MovieModel
     public MovieModel(){
         movieList = FXCollections.observableArrayList();
     }
+
+    public ObservableList<Movie> listToObservableList(){
+        List<Movie> tempMovieList = new ArrayList<Movie>();
+        tempMovieList = this.movieManager.getAllMovies();
+        for(Movie movie : tempMovieList){
+            movieList.add(movie);
+        }
+        return movieList;
+    }
+
+    public void createMovie(String title, String )
 }
