fd3939241f3a1ba845bcad555eeecce9de7dd707
kasp441 <91524039+kasp441@users.noreply.github.com>
2022-01-05 10:04:05 +0100

    created dabase connection

--- src/DAL/DatabaseDAO.java
+++ src/DAL/DatabaseDAO.java
@@ -1,7 +1,35 @@
 package DAL;
 
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.Properties;
+
 public class DatabaseDAO
 {
+    private static final String DB_SETTINGS = "DatabaseLogin.txt";
+    private SQLServerDataSource dataSource;
+
+    public DatabaseDAO() throws IOException {
+        Properties properties = new Properties();
+        properties.load(new FileInputStream(DB_SETTINGS));
+        dataSource = new SQLServerDataSource();
+        dataSource.setServerName(properties.getProperty("IpAddress"));
+        dataSource.setServerName(properties.getProperty("Database"));
+        dataSource.setServerName(properties.getProperty("Id"));
+        dataSource.setServerName(properties.getProperty("Password"));
+    }
+
+    public Connection getConnection() throws SQLServerException {
+        return dataSource.getConnection();
+    }
+
+    public static void main(String[] args) {
+
+    }
 
 }
 
--- src/DAL/MovieDAO.java
+++ src/DAL/MovieDAO.java
@@ -1,22 +1,10 @@
 package DAL;
 
-import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
-import com.microsoft.sqlserver.jdbc.SQLServerException;
-
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Properties;
-
 public class MovieDAO
 {
-    private static final String DB_SETTINGS = "DatabaeLogin.txt";
+    public MovieDAO(){
 
-    private SQLServerDataSource dataSource;
 
-    public MovieDAO() throws IOException {
-        Properties properties = new Properties();
-        properties.load(new FileReader(DB_SETTINGS));
     }
 
 }
