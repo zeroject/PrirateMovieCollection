63afc385509bcd9041dd6d6dd9df45236325f4ec
zeroject <47432671+zeroject@users.noreply.github.com>
2022-01-17 10:56:18 +0100

    Anders Please fix

--- src/MVC/UpdateMovieController.java
+++ src/MVC/UpdateMovieController.java
@@ -1,5 +1,22 @@
 package MVC;
 
+import MVC.Model.MovieModel;
+import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
+
+import java.io.IOException;
+
 public class UpdateMovieController
 {
+    MovieModel movieModel = new MovieModel();
+    @FXML
+    Slider slider = new Slider();
+
+    public UpdateMovieController() throws IOException {
+    }
+
+    public void updateNumber(){
+        float rating = (float) slider.getValue();
+        movieModel.updateMovie();
+    }
 }
