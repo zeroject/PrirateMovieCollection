0c72f65264aff62a657e1b1fb42cf40c9c432d77
kasp441 <91524039+kasp441@users.noreply.github.com>
2022-01-17 11:19:44 +0100

    bug hunting

--- src/BLL/MovieManager.java
+++ src/BLL/MovieManager.java
@@ -34,7 +34,7 @@ public class MovieManager
     public List<Movie> getAllMovies() {
         for (Movie movie : movieDAO.getAllMovies()) {
             movie.addCategoryToMovie(categoryDAO.getAllCategoryMovies(movie.getId()));
-            System.out.println(movie.getCategories().toString());
+            System.out.println(movie.getCategories().toString()+1);
         }
 
         return movieDAO.getAllMovies();
