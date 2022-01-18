4d27f8f29207a6de154b02c3b0687572cf24f6a4
kasp441 <91524039+kasp441@users.noreply.github.com>
2022-01-18 13:12:53 +0100

    update movieSearcher with movieRating(with bugs)

--- src/BLL/util/MovieSearcher.java
+++ src/BLL/util/MovieSearcher.java
@@ -12,7 +12,7 @@ public class MovieSearcher
         List<Movie> searchResult = new ArrayList<>();
 
         for (Movie movie : searchBase){
-            if(compareToTitle(query, movie) || compareToGenre(query, movie)){
+            if(compareToTitle(query, movie) || compareToGenre(query, movie) || compareToRating(query, movie)){
                 searchResult.add(movie);
             }
         }
@@ -25,7 +25,6 @@ public class MovieSearcher
 
     private boolean compareToGenre(String query, Movie movie) {
         boolean result = false;
-        System.out.println(movie.getCategories());
         if (movie.getCategories() != null)
         {
             for (Category category : movie.getCategories())
@@ -39,6 +38,36 @@ public class MovieSearcher
                      result = false;
                 }
             }
+        }
+        return result;
+    }
+
+    private boolean compareToRating(String query, Movie movie)
+    {
+        System.out.println("works?");
+        boolean result = false;
+        if (stringOrNumber(query))
+        {
+            System.out.println("works2?");
+            if (movie.getMovieRating() >= Float.parseFloat(query))
+            {
+                System.out.println("works3?");
+                result = true;
+            }
+            System.out.println(result);
+        }
+        return result;
+    }
+
+    private boolean stringOrNumber(String query)
+    {
+        boolean result = false;
+        String numbers = "0,1,2,3,4,5,6,,7,8,9";
+        String letters = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
+        System.out.println(query + query.contains(numbers));
+        if (query.contains(numbers) && !query.toLowerCase().contains(letters))
+        {
+            result = true;
         }
         return result;
     }
